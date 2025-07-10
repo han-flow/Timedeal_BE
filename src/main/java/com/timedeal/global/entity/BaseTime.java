6879ee24b3539f32f912eb54ac58b7a6c991aab8
HanGwonhui <jana32496@gmail.com>
2025-07-10 19:53:17 +0900

    feat: global entity 생성 (BaseEntity, BaseTime)

--- src/main/java/com/timedeal/global/entity/BaseTime.java
+++ src/main/java/com/timedeal/global/entity/BaseTime.java
@@ -1,16 +1,46 @@
 package com.timedeal.global.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 
 @Getter
 @MappedSuperclass
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public abstract class BaseTime extends BaseEntity{
+public abstract class BaseTime extends BaseEntity {
 
+    @CreatedDate
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH:mm:ss")
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH:mm:ss")
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        this.updatedAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+    }
 
 }
